2c20dfe1ee2b52dee1d5fa5af55cab89f3c117c3
Sukmafirlyy <sukmafirlyy@gmail.com>
2024-06-05 09:00:16 +0700

    layout baru

--- AdminDashboard/src/Home.jsx
+++ AdminDashboard/src/Home.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // File CSS untuk styling
 import { MdOutlineSpeed, MdTrain, MdLocationOn } from 'react-icons/md';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend, ZAxis } from 'recharts';
 import GaugeComponent from 'react-gauge-component';
 import io from 'socket.io-client';
 
 const MAX_DATA_COUNT = 20;
-const MAX_SPEED = 100;
+const MAX_SPEED = 350;
 const gaugeLimits = [
   { limit: 20, color: '#5BE12C', showTick: true },
   { limit: 40, color: '#F5CD19', showTick: true },
@@ -13,43 +14,16 @@ const gaugeLimits = [
   { limit: MAX_SPEED, color: '#EA4228', showTick: true },
 ];
 
-const staticSpeedData = [
-  { distance: 10, speed: 100 },
-  { distance: 20, speed: 85 },
-  { distance: 30, speed: 75 },
-  { distance: 40, speed: 65 },
-  { distance: 50, speed: 55 },
-  { distance: 60, speed: 0 },
-];
-
 function Home() {
   const [sensorData, setSensorData] = useState([]);
   const [socketConnected, setSocketConnected] = useState(false);
   const [gaugeValue, setGaugeValue] = useState(0);
   const [distanceData, setDistanceData] = useState([]);
+  const [totalDistance, setTotalDistance] = useState(0);
+  const [prevTimestamp, setPrevTimestamp] = useState(null);
 
   const kmhToMs = (value) => {
-    if (value >= 1) {
-      const msValue = value / 3.6;
-      return { value: msValue.toFixed(2), unit: 'm/s' };
-    } else {
-      return { value: value.toFixed(2), unit: 'km/h' };
-    }
-  };
-
-  const calculateDistance = (data) => {
-    let distance = 0;
-    const distanceData = data.map((point, index) => {
-      if (index === 0) {
-        return { ...point, distance };
-      }
-      const prevPoint = data[index - 1];
-      const timeDiff = (new Date(point.date) - new Date(prevPoint.date)) / 1000; // time difference in seconds
-      const speedMs = prevPoint.speed / 3.6; // convert speed to m/s
-      distance += speedMs * timeDiff; // distance in meters
-      return { ...point, distance: distance / 1000 }; // convert to kilometers
-    });
-    return distanceData;
+    return { value: value.toFixed(2), unit: 'km/h' };
   };
 
   useEffect(() => {
@@ -77,107 +51,124 @@ function Home() {
     });
 
     socket.on('sensorData', ({ value, date }) => {
+      const newTimestamp = new Date(date).getTime();
+
       setSensorData(prevData => {
-        const newData = [...prevData, { date, speed: value }].slice(-MAX_DATA_COUNT);
-        setDistanceData(calculateDistance(newData));
+        const newData = [...prevData, { date: newTimestamp, speed: value }].slice(-MAX_DATA_COUNT);
+
+        if (prevTimestamp !== null) {
+          const timeDiff = (newTimestamp - prevTimestamp) / 3600; // perbedaan waktu dalam jam
+          const incrementalDistance = value * timeDiff; // jarak dalam kilometer
+          setTotalDistance(prevDistance => {
+            const updatedDistance = prevDistance + incrementalDistance;
+            return parseFloat(updatedDistance.toFixed(2)); // memastikan dua angka di belakang koma
+          });
+
+          const updatedDistanceData = newData.map((point, index) => ({
+            ...point,
+            distance: (index === 0 ? 0 : (totalDistance + incrementalDistance)).toFixed(2),
+          }));
+
+          setDistanceData(updatedDistanceData);
+        }
+
+        setPrevTimestamp(newTimestamp);
         return newData;
       });
+
       setGaugeValue(value);
     });
 
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [prevTimestamp, totalDistance]);
 
   const lastSensorData = sensorData.length > 0 ? sensorData[sensorData.length - 1].speed : 0;
   const { value: convertedValue, unit: speedUnit } = kmhToMs(lastSensorData);
 
+  //console.log('distanceData:', distanceData);
+
   return (
     <main className='main-container'>
-      <div className='main-title'>
-        <h1>DASHBOARD</h1>
+      <div className='gauge-chart'style={{ flex: 1, maxWidth: '700px' }} >
+        <h3>Gauge Chart</h3>
+        <GaugeComponent
+          style={{ width: '100%', height: '80%'}} // Atur lebar dan tinggi di sini
+          arc={{
+            nbSubArcs: gaugeLimits.length,
+            colorArray: gaugeLimits.map(limit => limit.color),
+            width: 0.3,
+            padding: 0.003
+          }}
+          labels={{
+            valueLabel: {
+              fontSize: 40,
+              formatTextValue: value => `${value.toFixed(2)} km/h`
+            }
+          }}
+          value={gaugeValue}
+          maxValue={MAX_SPEED}
+        />
       </div>
       <div className='main-cards'>
         <div className='card'>
           <div className='card-inner'>
             <h3>Speed Rated</h3>
-            <MdOutlineSpeed className='card_icon' />
           </div>
           <div className="d-flex align-items-center">
             <h2 id='speedValue'>{convertedValue}</h2>
             <span className="unit">{speedUnit}</span>
           </div>
-          <small className="text-muted">Kecepatan yang Terbaca oleh Radar</small>
+          {/*<small className="text-muted">Kecepatan yang Terbaca oleh Radar</small>*/}
         </div>
         <div className='card'>
           <div className='card-inner'>
             <h3>Train Position</h3>
-            <MdTrain className='card_icon' />
           </div>
           <div className="d-flex align-items-center">
-            <h2>33</h2> {/* untuk dihubungkan dengan sensor */}
+            <h2>{totalDistance.toFixed(2)}</h2>
             <span className="unit">km</span>
           </div>
-          <small className="text-muted">Posisi terhadap Jarak</small>
+          {/*<small className="text-muted">Posisi terhadap Jarak</small>*/}
         </div>
         <div className='card'>
           <div className='card-inner'>
             <h3>Blocking</h3>
-            <MdLocationOn className='card_icon' />
           </div>
           <div className="d-flex align-items-center">
             <h2>42</h2> {/* untuk dihubungkan dengan sensor */}
           </div>
-          <small className="text-muted">Posisi Kereta</small>
+          {/*<small className="text-muted">Posisi Kereta</small>*/}
         </div>
       </div>
-      <div className='charts-wrapper'>
-        <div className='line-chart'>
-          <ResponsiveContainer width="100%" height="100%">
-            <h3>Grafik kecepatan terhadap jarak</h3>
-            <LineChart
-              width={500}
-              height={300}
-              data={distanceData}
-              margin={{
-                top: 5,
-                right: 30,
-                left: -30,
-                bottom: 5,
-              }}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="distance" label={{ value: "Jarak (km)", position: 'insideBottomRight', offset: 0 }} />
-              <YAxis label={{ value: "Kecepatan (m/s)", angle: -90, position: 'insideLeft' }} />
-              <Tooltip />
-              <Legend />
-              <Line type="monotone" dataKey="speed" stroke="#8884d8" activeDot={{ r: 8 }} />
-              <Line type="monotone" dataKey="speed" data={staticSpeedData} stroke="green" />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-        <div className='gauge-chart' style={{ flex: 1, maxWidth: '500px' }}>
-          <h3>Gauge Chart</h3>
-          <GaugeComponent
-            arc={{
-              nbSubArcs: gaugeLimits.length,
-              colorArray: gaugeLimits.map(limit => limit.color),
-              width: 0.3,
-              padding: 0.003
-            }}
-            labels={{
-              valueLabel: {
-                fontSize: 40,
-                formatTextValue: value => kmhToMs(value).value + ' ' + kmhToMs(value).unit
-              }
-            }}
-            value={gaugeValue}
-            maxValue={MAX_SPEED}
-          />
-        </div>
+      <div className='button'>
+        <button onClick={() => alert('Navigasi ke halaman history')}>Go to History</button>
+      </div>
+      <div className='line-chart'>
+          <h3>Grafik kecepatan terhadap jarak</h3>
+          <ResponsiveContainer width="95%" height={300} margin-bottom="-4px"> {/* Atur lebar dan tinggi di sini */}
+          <LineChart
+            data={distanceData}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 5,
+              bottom: 9,
+            }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="distance" label={{ value: "Distance (km)", position: 'insideBottomRight', offset: 0 }} />
+            <YAxis label={{ value: "speed (km/h)", angle: -90, position: 'insideLeft' }}
+              domain={[0, 350]} // Set Y-axis range from 0 to 350
+              ticks={[0, 50, 100, 150, 200, 250, 300, 350]} // Set Y-axis ticks with a gap of 50
+            />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="speed" stroke="#8884d8" activeDot={{ r: 8 }} />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </main>
   );
 }
-
 export default Home;
